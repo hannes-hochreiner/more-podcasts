c4c26b773fcaba5764c6aa445ec6ff504f471b79
Hannes Hochreiner <hannes@hochreiner.net>
2017-10-29 12:53:37 +0100

    bug fix

--- src/ChannelService.js
+++ src/ChannelService.js
@@ -10,7 +10,7 @@ export default class ChannelService {
   _getApiChannels(topic, data) {
     pps('system.getIdToken').then(idToken => {
       return axios.get('https://internal.hochreiner.net/rss-json-service/channels', {
-        headers: {'Authorization': `Bearer ${idToken}`}
+        headers: {'Authorization': `Bearer ${idToken.idToken}`}
       });
     }).then(res => {
       console.dir(res);
