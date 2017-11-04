41ffb294713671954c9e56044b1016e735b28350
Hannes Hochreiner <hannes@hochreiner.net>
2017-11-04 16:55:48 +0100

    fixed a bug in the tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import np from './NotificationPresenter';
+import ps from './PubSub';
+import uuid from 'uuid';
+import PubSub from 'pubsub-js';
+
+ps.ps = PubSub;
+ps.uuid = uuid;
+
+np.ps = ps;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
