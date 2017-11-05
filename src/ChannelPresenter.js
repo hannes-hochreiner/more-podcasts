6f411f58a82264aeb66e9dcf6b2bacd8ee371da5
Hannes Hochreiner <hannes@hochreiner.net>
2017-11-05 11:17:00 +0100

    improved channel page

--- src/ChannelPresenter.js
+++ src/ChannelPresenter.js
@@ -4,32 +4,71 @@ import {promisedPubSub as pps} from './utils';
 export default class ChannelPresenter {
   constructor(view) {
     this._view = view;
-    this._updateItems();
-  }
-
-  showItem(itemId) {
-
+    this._updateState();
   }
 
   goToChannelListPage() {
     PubSub.publish('system.goToChannelListPage.request');
   }
 
+  removeDownload(channelId, itemId) {
+    return pps('system.removeEnclosureDocAndBinaryByChannelItemId', {channelId: channelId, itemId: itemId}).then(() => {
+      return this._updateState();
+    });
+  }
+
+  refreshDownload(channelId, itemId) {
+    return pps('system.removeEnclosureBinaryByChannelIdItemId', {channelId: channelId, itemId: itemId}).then(() => {
+      return this._updateState();
+    });
+  }
+
   requestDownload(channelId, itemId) {
     pps('system.addOrUpdateEnclosureDoc', {
       enclosureDoc: {channelId: channelId, itemId: itemId}
     }).then(() => {
-      return pps('system.getEnclosureDocsByChannelId', {channelId: channelId});
+      return pps('system.getItemByChannelIdId', {channelId: channelId, id: itemId});
     }).then(res => {
-      // TODO: update items
+      res.item.triaged = true;
+      return pps('system.addOrUpdateItem', {item: res.item});
+    }).then(() => {
+      return this._updateState();
     });
   }
 
-  _updateItems() {
-    pps('system.getItemsByChannelId', {channelId: this._view.channelId}).then(res => {
-      this._view.items = res.items.sort((i1, i2) => {
+  play(channelId, itemId) {
+    return pps('system.getItemByChannelIdId', {channelId: channelId, id: itemId}).then(res => {
+      return pps('system.playerService.setItem', {item: res.item});
+    }).then(() => {
+      return pps('system.playerService.setPlaying', {playing: true});
+    }).then(() => {
+      return this._updateState();
+    });
+  }
+
+  toggleTriaged(channelId, itemId) {
+    return pps('system.getItemByChannelIdId', {channelId: channelId, id: itemId}).then(res => {
+      if (res.item.triaged) {
+        delete res.item.triaged;
+      } else {
+        res.item.triaged = true;
+      }
+
+      return pps('system.addOrUpdateItem', {item: res.item});
+    }).then(() => {
+      return this._updateState();
+    });
+  }
+
+  _updateState() {
+    return Promise.all([
+      pps('system.getItemsByChannelId', {channelId: this._view.channelId}),
+      pps('system.getEnclosureDocsByChannelId', {channelId: this._view.channelId})
+    ]).then(res => {
+      this._view.items = res[0].items.sort((i1, i2) => {
         return -i1.date.localeCompare(i2.date);
       });
+      this._view.enclosureDocs = res[1].enclosureDocs;
     });
   }
 
--- src/ChannelView.js
+++ src/ChannelView.js
@@ -6,22 +6,30 @@ import RaisedButton from 'material-ui/RaisedButton';
 import ChannelPresenter from './ChannelPresenter';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import Chip from 'material-ui/Chip';
-import New from 'material-ui/svg-icons/av/new-releases';
 import IconButton from 'material-ui/IconButton';
+import NewIcon from 'material-ui/svg-icons/av/new-releases';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import AppBar from 'material-ui/AppBar';
+import Avatar from 'material-ui/Avatar';
 import BackIcon from 'material-ui/svg-icons/navigation/arrow-back';
 
 export default class ChannelView extends Component {
   state = {
-    items: []
+    items: [],
+    enclosureDocs: []
   };
 
   set items(value) {
     this.setState({
       items: value
+    });
+  }
+
+  set enclosureDocs(value) {
+    this.setState({
+      enclosureDocs: value
     });
   }
 
@@ -37,12 +45,38 @@ export default class ChannelView extends Component {
     this._pres.finalize();
   }
 
+  _play(channelId, itemId) {
+    this._pres.play(channelId, itemId);
+  }
+
+  _refreshDownload(channelId, itemId) {
+    this._pres.refreshDownload(channelId, itemId);
+  }
+
   _requestDownload(channelId, itemId) {
     this._pres.requestDownload(channelId, itemId);
   }
 
-  _handleListItemClick(id) {
-    this._pres.showItem(id);
+  _removeDownload(channelId, itemId) {
+    this._pres.removeDownload(channelId, itemId);
+  }
+
+  _toggleNew(channelId, itemId) {
+    this._pres.toggleTriaged(channelId, itemId);
+  }
+
+  _getItemStatusString(item) {
+    let str = '';
+
+    if (item.currentTime) {
+      str += 'partially played; ';
+    }
+
+    if (item.playCount) {
+      str += `${item.playCount} times played; `;
+    }
+
+    return str;
   }
 
   render() {
@@ -63,20 +97,72 @@ export default class ChannelView extends Component {
         <AppBar title="channel" showMenuIconButton="false" iconElementLeft={backNavigation}/>
         <List>
           {this.state.items.map(item => {
+            let menuEntries = [{label: 'toggle new', onClick: this._toggleNew.bind(this, item.channelId, item.id)}];
+
+            const chipStyle = {margin: 4};
+
+            let patiallyPlayedChip;
+
+            if (item.currentTime) {
+              patiallyPlayedChip = (
+                <Chip style={chipStyle}>partially played</Chip>
+              );
+            }
+
+            let playCountChip;
+
+            if (item.playCount) {
+              playCountChip = (
+                <Chip style={chipStyle}><Avatar>{item.playCount}</Avatar>times played</Chip>
+              );
+            }
+
+            let downloadChip;
+
+            if (this.state.enclosureDocs) {
+              let enclosureDoc = this.state.enclosureDocs.find(encDoc => {
+                return encDoc.itemId === item.id && encDoc.channelId === item.channelId;
+              });
+
+              if (enclosureDoc) {
+                let downloadStatus = 'requested';
+
+                if (enclosureDoc._attachments) {
+                  downloadStatus = 'available';
+                  menuEntries.push({label: 'refresh download', onClick: this._refreshDownload.bind(this, item.channelId, item.id)});
+                  menuEntries.push({label: 'play', onClick: this._play.bind(this, item.channelId, item.id)});
+                }
+
+                menuEntries.push({label: 'remove download', onClick: this._removeDownload.bind(this, item.channelId, item.id)});
+
+                downloadChip = (
+                  <Chip style={chipStyle}>download {downloadStatus}</Chip>
+                );
+              } else {
+                menuEntries.push({label: 'request download', onClick: this._requestDownload.bind(this, item.channelId, item.id)});
+              }
+            }
+
+            let newChip;
+
+            if (!item.triaged) {
+              newChip = (
+                <Chip style={chipStyle}><Avatar><NewIcon/></Avatar>new</Chip>
+              );
+            }
+
             const rightIconMenu = (
               <IconMenu iconButtonElement={iconButtonElement}>
-                <MenuItem onClick={this._requestDownload.bind(this, item.channelId, item.id)}>download</MenuItem>
-                <MenuItem>toggle new</MenuItem>
-                <MenuItem>play</MenuItem>
+                {menuEntries.map(ent => {
+                  return (<MenuItem key={ent.label} onClick={ent.onClick}>{ent.label}</MenuItem>);
+                })}
               </IconMenu>
             );
 
             return <ListItem
               key={item.id}
               rightIconButton={rightIconMenu}
-              leftIcon={<New/>}
-              primaryText={item.title}
-              onClick={this._handleListItemClick.bind(this, item.id)}
+              primaryText={<div>{item.title}<div style={{display: 'flex', flexWrap: 'wrap',}}>{newChip}{patiallyPlayedChip}{playCountChip}{downloadChip}</div></div>}
             />;
           })}
         </List>
