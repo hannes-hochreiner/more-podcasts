dbecd496f6383158982428ca2af2fc8c187878e9
travis-ci <travis@travis-ci.org>
2019-08-19 19:03:30 +0000

    Lastest version on successful travis build 95 auto-pushed to gh-pages

---
+++ precache-manifest.4a90ea60b176e2268ab87b13b9f06dcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac25c76b96a5e04741b0368ab7b3ef7b",
+    "url": "/more-podcasts/index.html"
+  },
+  {
+    "revision": "af012671595268879f8e",
+    "url": "/more-podcasts/static/css/main.b89face1.chunk.css"
+  },
+  {
+    "revision": "bd2fee204e44ec256cf1",
+    "url": "/more-podcasts/static/js/2.db5cd20c.chunk.js"
+  },
+  {
+    "revision": "af012671595268879f8e",
+    "url": "/more-podcasts/static/js/main.d3181c39.chunk.js"
+  },
+  {
+    "revision": "018a262e6e786eed12fc",
+    "url": "/more-podcasts/static/js/runtime~main.bbc6b087.js"
+  }
+]);
