71bed3b44604d8af018ba381bf31d43e98d67553
Hannes Hochreiner <hannes@hochreiner.net>
2021-06-26 16:03:25 +0200

    start rewrite

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-[![Build Status](https://travis-ci.org/hannes-hochreiner/more-podcasts.svg?branch=master)](https://travis-ci.org/hannes-hochreiner/more-podcasts)
-[![sonar cloud status](https://sonarcloud.io/api/project_badges/measure?project=more-podcasts&metric=alert_status)](https://sonarcloud.io/dashboard?id=more-podcasts)
-
 # more-podcasts
 A simple offline-first app for listening to podcasts.
---
+++ src/components/channel_list.rs
@@ -0,0 +1,105 @@
+use crate::objects::channel::Channel;
+use yew::{
+    format::{Json, Nothing},
+    prelude::*,
+    services::fetch::{FetchService, FetchTask, Request, Response},
+};
+use anyhow::Error;
+
+pub struct ChannelList {
+    link: ComponentLink<Self>,
+    fetch_task: Option<FetchTask>,
+    channels: Option<Vec<Channel>>,
+    error: Option<Error>,
+}
+
+pub enum Msg {
+    ReceiveChannels(Result<Vec<Channel>, anyhow::Error>),
+}
+
+impl ChannelList {
+    fn view_channel_list(&self) -> Html {
+        match &self.channels {
+            Some(c) => {
+                html! {<ul>
+                    { c.iter().map(|i| html! { <li><img src={i.image.clone()} width="50" height="50"/>{&i.title}</li> }).collect::<Html>() }
+                </ul>}
+            },
+            None => html! { <p> {"no channels available"} </p> }
+        }
+    }
+
+    fn view_fetching(&self) -> Html {
+        if self.fetch_task.is_some() {
+            html! { <p>{ "Fetching data..." }</p> }
+        } else {
+            html! { <p></p> }
+        }
+    }
+
+    fn view_error(&self) -> Html {
+        match &self.error {
+            Some(e) => html! { <p>{e}</p> },
+            None => html! {}
+        }
+    }
+}
+
+impl Component for ChannelList {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        let request = Request::get("/api/channels")
+        .body(Nothing)
+        .expect("Could not build request.");
+        // 2. construct a callback
+        let callback =
+        link
+            .callback(|response: Response<Json<Result<Vec<Channel>, anyhow::Error>>>| {
+                let Json(data) = response.into_body();
+                Msg::ReceiveChannels(data)
+            });
+        // 3. pass the request and callback to the fetch service
+        let task = FetchService::fetch(request, callback).expect("failed to start request");
+
+        Self {
+            link,
+            fetch_task: Some(task),
+            channels: None,
+            error: None,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::ReceiveChannels(res) => {
+                match res {
+                    Ok(c) => {
+                        self.channels = Some(c);
+                    },
+                    Err(e) => {
+                        self.error = Some(e);
+                    },
+                }
+
+                self.fetch_task = None;
+                true
+            },
+        }
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <>
+                { self.view_fetching() }
+                { self.view_channel_list() }
+                { self.view_error() }
+            </>
+        }
+    }
+}
